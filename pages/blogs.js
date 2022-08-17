1a238484306cab096264cc28eb0172ec010c88fb
navyansh007 <navyanshkesarwani@gmail.com>
2022-08-17 12:39:43 +0530

    Fixed Blog Page Title Issue

--- pages/blogs.js
+++ pages/blogs.js
@@ -2,9 +2,14 @@ import React from 'react'
 import Blogs from '../components/blogs' 
 import { createClient } from 'next-sanity'
 import styles from '../styles/Home.module.css'
+import Head from 'next/head'
+
 const Blogposts = ({posts}) => {
   return (
     <>
+      <Head>
+        <title>Blogs</title>
+      </Head>
       <div className={styles.container}>
         <main className={styles.main}>
           <Blogs posts={posts}/>
